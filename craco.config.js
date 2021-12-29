9fbd0d549d13c7e776d077255a23868b0258b324
lizhixuan <1031760551@qq.com>
2021-12-29 17:31:03 +0800

    feat: 新增proxy跨域配置

--- craco.config.js
+++ craco.config.js
@@ -22,5 +22,16 @@ module.exports = {
         }
       }
     }
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7001',
+        secure: false,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
